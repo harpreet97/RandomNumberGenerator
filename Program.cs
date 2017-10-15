5ab11dc71e067e001a1e0032330eeb08c46ce79e
harpreet97 <harrycheema97@yahoo.com>
2017-10-16 11:08:33 +1300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace randomnumbergeneration
+{
+    class Program
+    {
+        static List<string> cars = new List<string>{"Audi", "MAzda", "Toyota", "Nissan"};
+        static List<string> drivers = new List<string>{"Bob", "Lisa", "Tim", "Simon", "Kevin"};
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DisplayDriver());
+            Console.WriteLine(DisplayDriver());
+            Console.WriteLine(DisplayDriver());
+
+        }
+
+        static int GenerateRandomNumber(List<string> myList){
+            var rand = new Random();
+            return rand.Next(1, myList.Count);
+
+        }
+        static string DisplayDriver(){
+
+            var selectedCar = cars[GenerateRandomNumber(cars)];
+            var selectedDriver = drivers[GenerateRandomNumber(drivers)];
+            
+            return $"{selectedDriver} is driving the {selectedCar}";
+        }
+    }
+}
